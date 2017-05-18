95ae1e7bf2f0c4853f83e40e5de5a9d6864ac174
Aleksandra Matiyev <aleksa000777@mail.ru>
2017-05-17 22:57:17 -0400

    broken by build xcode

--- index.ios.js
+++ index.ios.js
@@ -1,38 +1,90 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React from 'react';
 import {
   AppRegistry,
+  Image,
+  StatusBar,
   StyleSheet,
-  Text,
+  TouchableOpacity,
   View,
-  Button,
-  Alert,
-  TextInput,
 } from 'react-native';
+import Camera from 'react-native-camera';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  overlay: {
+    position: 'absolute',
+    padding: 16,
+    right: 0,
+    left: 0,
+    alignItems: 'center',
+  },
+  topOverlay: {
+    top: 0,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  bottomOverlay: {
+    bottom: 0,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  captureButton: {
+    padding: 15,
+    backgroundColor: 'white',
+    borderRadius: 40,
+  },
+  typeButton: {
+    padding: 5,
+  },
+  flashButton: {
+    padding: 5,
+  },
+  buttonsSpace: {
+    width: 10,
+  },
+});
 
-
-export default class WhoIsThis extends Component {
+export default class WhoIsThis extends React.Component {
   constructor(props) {
     super(props);
+
+    this.camera = null;
+
     this.state = {
-      result: {},
-      text: ''
+      camera: {
+        aspect: Camera.constants.Aspect.fill,
+        captureTarget: Camera.constants.CaptureTarget.cameraRoll,
+        type: Camera.constants.Type.back,
+        orientation: Camera.constants.Orientation.auto,
+        flashMode: Camera.constants.FlashMode.auto,
+      },
+      isRecording: false,
+      result: {}
     };
-    this.onButtonPress = this.onButtonPress.bind(this)
   }
 
- onButtonPress() {
-      this.getMoviesFromApiAsync();
+  takePicture = () => {
+    if (this.camera) {
+      this.camera.capture()
+        .then((data) => console.log(data))
+        .then(this.getMoviesFromApiAsync())
+        .catch(err => console.error(err));
+    }
   }
 
   getMoviesFromApiAsync () {
-    let url = this.state.text
+    let url = 'http://www.celebritybeliefs.com/wp-content/uploads/2016/07/matt-damon-religion-hobbies-political-views.jpg'
     console.log('url', url);
     fetch("http://localhost:3000/" + url, {method: "GET"})
           .then((response) => response.json())
@@ -43,47 +95,152 @@ export default class WhoIsThis extends Component {
           }).done()
   }
 
+  startRecording = () => {
+    if (this.camera) {
+      this.camera.capture({mode: Camera.constants.CaptureMode.video})
+          .then((data) => console.log(data))
+          .catch(err => console.error(err));
+      this.setState({
+        isRecording: true
+      });
+    }
+  }
+
+  stopRecording = () => {
+    if (this.camera) {
+      this.camera.stopCapture();
+      this.setState({
+        isRecording: false
+      });
+    }
+  }
+
+  switchType = () => {
+    let newType;
+    const { back, front } = Camera.constants.Type;
+
+    if (this.state.camera.type === back) {
+      newType = front;
+    } else if (this.state.camera.type === front) {
+      newType = back;
+    }
+
+    this.setState({
+      camera: {
+        ...this.state.camera,
+        type: newType,
+      },
+    });
+  }
+
+  get typeIcon() {
+    let icon;
+    const { back, front } = Camera.constants.Type;
+
+    if (this.state.camera.type === back) {
+      icon = require('./assets/ic_camera_front_white.png');
+    } else if (this.state.camera.type === front) {
+      icon = require('./assets/ic_camera_front_white.png');
+    }
+
+    return icon;
+  }
+
+  switchFlash = () => {
+    let newFlashMode;
+    const { auto, on, off } = Camera.constants.FlashMode;
+
+    if (this.state.camera.flashMode === auto) {
+      newFlashMode = on;
+    } else if (this.state.camera.flashMode === on) {
+      newFlashMode = off;
+    } else if (this.state.camera.flashMode === off) {
+      newFlashMode = auto;
+    }
+
+    this.setState({
+      camera: {
+        ...this.state.camera,
+        flashMode: newFlashMode,
+      },
+    });
+  }
+
+  get flashIcon() {
+    let icon;
+    const { auto, on, off } = Camera.constants.FlashMode;
+
+    if (this.state.camera.flashMode === auto) {
+      icon = require('./assets/ic_camera_front_white.png');
+    } else if (this.state.camera.flashMode === on) {
+      icon = require('./assets/ic_camera_front_white.png');
+    } else if (this.state.camera.flashMode === off) {
+      icon = require('./assets/ic_camera_front_white.png');
+    }
+
+    return icon;
+  }
 
   render() {
-    console.log('hello');
+    console.log('this.state', this.state.result);
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          {this.state.result.length > 0 ? this.state.result: 'hello'}
-        </Text>
-        <TextInput
-          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-          onChangeText={(text) => this.setState({text})}
-          value={this.state.text}
+        <StatusBar
+          animated
+          hidden
         />
-        <Button
-          onPress={this.onButtonPress}
-          title="Learn More"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={this.state.camera.aspect}
+          captureTarget={this.state.camera.captureTarget}
+          type={this.state.camera.type}
+          flashMode={this.state.camera.flashMode}
+          onFocusChanged={() => {}}
+          onZoomChanged={() => {}}
+          defaultTouchToFocus
+          mirrorImage={false}
         />
+        <View style={[styles.overlay, styles.topOverlay]}>
+          <TouchableOpacity
+            style={styles.typeButton}
+            onPress={this.switchType}
+          >
+            <Image
+              source={this.typeIcon}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.flashButton}
+            onPress={this.switchFlash}
+          >
+            <Image
+              source={this.flashIcon}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={[styles.overlay, styles.bottomOverlay]}>
+          {
+            !this.state.isRecording
+            &&
+            <TouchableOpacity
+                style={styles.captureButton}
+                onPress={this.takePicture}
+            >
+              <Image
+                  source={require('./assets/ic_camera_front_white.png')}
+              />
+            </TouchableOpacity>
+            ||
+            null
+          }
+          <View style={styles.buttonsSpace} />
+        </View>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('WhoIsThis', () => WhoIsThis);
