56d4f0c02066a9da430f31da56160dcf99fa27bd
Aleksandra Matiyev <aleksa000777@mail.ru>
2017-06-14 17:40:44 -0400

    handle 2 api calls

--- index.ios.js
+++ index.ios.js
@@ -6,8 +6,10 @@ import {
   StyleSheet,
   TouchableOpacity,
   View,
+  Dimensions,
   Text
 } from 'react-native';
+import Modal from 'react-native-modal'
 import Camera from 'react-native-camera';
 const RNFS = require('react-native-fs');
 const xhr = new XMLHttpRequest();
@@ -41,19 +43,25 @@ export default class WhoIsThis extends React.Component {
       },
       isRecording: false,
       result: {},
-      path: ''
+      path: '',
+      isModalVisible: false
     };
   }
 
   takePicture = () => {
     if (this.camera) {
       this.camera.capture()
-        .then((data) => this.setState({path : data.path}, () => {
+        .then((data) => this.setState({
+            path : data.path,
+            result: {}
+          }, () => {
           this.uploadImage()
         }))
         .catch(err => console.error(err));
     }
   }
+
+  _hideModal = () => this.setState({ isModalVisible: false })
 
 // front or back camera
   switchType = () => {
@@ -89,10 +97,12 @@ export default class WhoIsThis extends React.Component {
 
   // image upload
   async uploadImage() {
+    // if back camera use celebrity API : front - demograthica
+    let apiV = this.state.camera.type === Camera.constants.Type.back ? "celebrity" : "demographics"
     let base64image = await RNFS.readFile(this.state.path, 'base64');
     let formData = new FormData();
     formData.append("csv", base64image);
-    fetch(uri,
+    fetch(uri + '?v=' + apiV,
       {
           method: "POST",
           headers: {
@@ -103,78 +113,119 @@ export default class WhoIsThis extends React.Component {
       })
       .then((response) => response.json())
       .then((response) => {
-            this.formattedResult(response.result)
+            this.setState({isModalVisible: true})
+            this.formattedResult(response.result, apiV)
           }).catch(function(err) {
             console.log("error", err);
         });
   }
 
-  formattedResult = (response) => {
-    let result = "";
-    for (var i = 0; i < response.length; i++){
-      result+= response[i].data.face.identity.concepts[0].name + " | "
+  formattedResult = (response, apiV) => {
+    if(apiV === "celebrity"){
+      let name = "";
+      for (var i = 0; i < response.length; i++){
+        name+= response[i].data.face.identity.concepts[0].name + "\n"
+      }
+      const result = this.state.result;
+      result.name = name;
+      this.setState({
+          result,
+      });
+    } else {
+      const result = this.state.result;
+      result.age = response[0].data.face.age_appearance.concepts[0].name;
+      result.gender = response[0].data.face.gender_appearance.concepts[0].name;
+      result.race = response[0].data.face.multicultural_appearance.concepts[0].name;
+      this.setState({
+          result,
+      });
     }
-    this.setState({result : result})
   }
 
   render() {
     return (
-        <View style={styles.container}>
-          <StatusBar
-            animated
-            hidden
-          />
-          <Camera
-            ref={(cam) => {
-              this.camera = cam;
-            }}
-            style={styles.preview}
-            aspect={this.state.camera.aspect}
-            captureTarget={this.state.camera.captureTarget}
-            type={this.state.camera.type}
-            flashMode={this.state.camera.flashMode}
-            onFocusChanged={() => {}}
-            onZoomChanged={() => {}}
-            defaultTouchToFocus
-            mirrorImage={false}
-          />
-          <View style={[styles.overlay, styles.topOverlay]}>
-            <TouchableOpacity
-              style={styles.typeButton}
-              onPress={this.switchType}
-            >
-              <Image
-                source={this.typeIcon}
-              />
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.flashButton}
-              onPress={this.switchFlash}
-            >
-            </TouchableOpacity>
-          </View>
-          <View style={[styles.overlay, styles.bottomOverlay]}>
-            <TouchableOpacity
-                style={styles.captureButton}
-                onPress={this.takePicture}
-            >
-              <Image
-                  source={require('./assets/ic_camera_front_white.png')}
-              />
-            </TouchableOpacity>
-            <View style={styles.buttonsSpace} />
-          </View>
-          <Text>{this.state.result.length > 0 ? this.state.result : ''}</Text>
+      <View style={styles.container}>
+        <StatusBar
+          animated
+          hidden
+        />
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={this.state.camera.aspect}
+          captureTarget={this.state.camera.captureTarget}
+          type={this.state.camera.type}
+          flashMode={this.state.camera.flashMode}
+          onFocusChanged={() => {}}
+          onZoomChanged={() => {}}
+          defaultTouchToFocus
+          mirrorImage={false}
+        />
+        <View style={[styles.overlay, styles.topOverlay]}>
+          <TouchableOpacity
+            style={styles.typeButton}
+            onPress={this.switchType}
+          >
+            <Image
+              source={this.typeIcon}
+            />
+          </TouchableOpacity>
         </View>
+        <View>
+          <Modal isVisible={this.state.isModalVisible}>
+            <View style={styles.modal}>
+              <TouchableOpacity onPress={this._hideModal} style={styles.hideModal}>
+                <Text>{this.state.result &&
+                  this.state.result.name ? this.state.result.name : 'Age:'+ this.state.result.age + '\n' + 'Gender:' + this.state.result.gender + '\n' +'Race:' + this.state.result.race
+                }</Text>
+              </TouchableOpacity>
+            </View>
+          </Modal>
+        </View>
+        <View style={[styles.overlay, styles.bottomOverlay]}>
+          <TouchableOpacity
+              style={styles.captureButton}
+              onPress={this.takePicture}
+          >
+            <Image
+                source={require('./assets/ic_camera_front_white.png')}
+            />
+          </TouchableOpacity>
+          <View style={styles.buttonsSpace} />
+        </View>
+      </View>
     );
   }
 }
 
 AppRegistry.registerComponent('WhoIsThis', () => WhoIsThis);
-
+var windowSize = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  modal: {
+    position: 'absolute',
+    top: windowSize.height/4,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: windowSize.height/3,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'rgba(255,255,255,0.9)',
+  },
+  hideModal: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'rgba(255,255,255,0.9)',
   },
   preview: {
     flex: 1,
